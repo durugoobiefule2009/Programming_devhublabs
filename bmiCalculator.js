263c3785af1427e4df58405741505749b4d03ccb
Durugo Ikechukwu <durugoobiefule2009@gmail.com>
2026-05-09 14:37:45 +0100

    Added new files: bmiCalculator and lifeinWeeks

---
+++ bmiCalculator.js
@@ -0,0 +1,9 @@
+function bmiCalculator(weight, height){
+    var weight = document.getElementById("weight").value;
+    var height = document.getElementById("height").value;
+    var bmiCalc = Math.round(weight / (height**2));
+
+    document.getElementById("result").innerHTML = "Your BMI is: " + bmiCalc;
+}
+
+
---
+++ lifeInWeeks.js
@@ -0,0 +1,10 @@
+function lifeInWeeks(age) {
+    var age = document.getElementById("age").value;
+    var time = 90 - age;
+    var days = 365 * time;
+    var weeks = 52 * time;
+    var months = 12 * time;
+
+    document.getElementById("result").innerHTML = "You have " + days + " days, " + weeks + " weeks, and " + months + " months left.";
+}
+
